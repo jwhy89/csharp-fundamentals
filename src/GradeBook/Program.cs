e5f6d8b1021b19807d790a3b18222982f6797813
Jarvis Yang <jarvisgyang@gmail.com>
2021-04-04 18:47:29 -0500

    Inital commit. Adding gitignore file.

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new double[3];
+            numbers[0] = 12.7;
+            numbers[1] = 10.3;
+            numbers[2] = 6.11;
+
+            var result = numbers[0];
+            result += numbers[1];
+            result += numbers[2];
+            System.Console.WriteLine(result);
+
+            if(args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
