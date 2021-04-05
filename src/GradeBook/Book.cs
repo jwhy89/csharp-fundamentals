22a26e001762958b97a3122324d6122d0c28ba99
Jarvis Yang <jarvisgyang@gmail.com>
2021-04-04 20:40:30 -0500

    Adding constructor and field definition.

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,10 +1,22 @@
+using System.Collections.Generic;
 namespace GradeBook
 {
     class Book
     {
+        // implicit constructor
+        public Book()
+        {
+            grades = new List<double>();
+        }
         public void AddGrade(double grade)
         {
-            
+            grades.Add(grade);
         }
+
+        // field definition option 1
+        // List<double> grades = new List<double>();
+
+        // field definition option 2
+        List<double> grades;
     }
 }
